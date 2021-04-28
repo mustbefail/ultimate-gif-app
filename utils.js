079a2a120af62a3f36033f01228ef63b273eeedb
Viktor Kushnir <mustbefail@gmail.com>
2021-04-28 21:04:41 +0300

    fix utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 export const getGiphyReqUrl = (action, searchParams = {}) => {
   const api_key = process.env.GIPHY_API_KEY;
-  const baseUrl = 'https://api.giphy.com/v1/gifs/';
+  const baseUrl = 'https://api.giphy.com/';
   const basePath = `/v1/gifs/${action}`;
   const result = new URL(basePath, baseUrl);
   const baseSearchParams = {
-    api_key: api_key,
+    api_key,
     q: '',
     limit: 25,
     rating: 'g',
