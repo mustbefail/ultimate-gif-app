c6a55e4b0b0bbf82525d6a8ec01e58e7db8552e4
Viktor Kushnir <mustbefail@gmail.com>
2021-05-28 21:24:56 +0300

    add switch/case for conditional render

--- src/components/App.js
+++ src/components/App.js
@@ -2,30 +2,49 @@
 /** @jsxFrag createFragment */
 import { createElement, useEffect, useState } from '../framework';
 import Navigation from './Navigation/Navigation';
-import GifsContainer from './Gifs/GifsContainer';
-import SearchGif from './Navigation/SearchGif';
-import { useGif } from '../getGifDataHook';
+import SearchedGifsContainer from './Gifs/SearchedGifsContainer';
+import CategoryContainer from './Categories';
+import SearchGif from './SearchGif';
+import Footer from './Footer';
+import Trends from './Trends';
+import RandomGif from './RandomGif';
 
 export default function App() {
-  const {
-    action,
-    setAction,
-    setSearchParams,
-    error,
-    dataLoading,
-    gifData,
-  } = useGif();
+  const [route, setRoute] = useState('trending');
+  const [action, setAction] = useState('');
+  const [newLoad, setNewLoad] = useState(null);
+
+  let content;
+  switch (route) {
+    case 'random':
+      content = <RandomGif action={action} newLoad={newLoad} />;
+      break;
+    case 'categories':
+      content = <CategoryContainer />;
+      break;
+    case 'search':
+      content = <SearchedGifsContainer action={action} />;
+      break;
+    case 'trending':
+      content = <Trends action={action} test={test} setTest={setTest} />;
+      break;
+    default:
+      content = <Trends />;
+      break;
+  }
 
   return (
-    <div className={`container`}>
-      <Navigation />
-      <SearchGif onChange={setSearchParams} setAction={setAction} />
-      <GifsContainer
-        action={action}
-        error={error}
-        dataLoading={dataLoading}
-        gifData={gifData}
+    <div className={`container d-flex flex-column h-100`}>
+      <Navigation
+        setRoute={setRoute}
+        setAction={setAction}
+        setNewLoad={setNewLoad}
+        newLoad={newLoad}
       />
+      <SearchGif setRoute={setRoute} setAction={setAction} />
+
+      {content}
+      <Footer />
     </div>
   );
 }
