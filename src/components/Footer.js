8ac6a0e372015de6dcf4e982f1a0ec2cffb37371
Viktor Kushnir <mustbefail@gmail.com>
2021-06-12 18:11:59 +0300

    add giphy link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className={`footer mt-auto text-center pt-5`}>
-      <span className={`text-light`}>Powered by GIPHY</span>
+      <span className={`text-light`}>
+        Powered by <a href="https://giphy.com/">GIPHY</a>
+      </span>
     </footer>
   );
 }
