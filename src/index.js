83d5bff707223896a27eab907a3837d99a1675c7
Viktor Kushnir <mustbefail@gmail.com>
2021-06-17 11:40:57 +0300

    router fix for gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import React from 'react';
 import App from './components/App';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('app'),
